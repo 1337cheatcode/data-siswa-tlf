75be15d3d45bb869b3a5555783646ec83a4c2b18
cheatcode <dwikurnia.cheatcode@gmail.com>
2023-11-29 10:25:12 +0700

    remove qwik

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import qwikdev from "@qwikdev/astro";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev()]
+  integrations: []
 });
